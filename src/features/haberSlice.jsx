efb15360c18622f3fd7feb1e4851548c465cb81b
ozkan <ozkandnz09@gmail.com>
2025-02-08 20:06:37 +0300

    return

--- src/features/haberSlice.jsx
+++ src/features/haberSlice.jsx
@@ -1,24 +1,23 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const getData =  createAsyncThunk('haberSlice/getData', async()=>{
-   const response =  await axios("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=81a4163ea7eb4bccb489151972100adb")
-})
+export const getData = createAsyncThunk("haberSlice/getData", async () => {
+  const response = await axios(
+    "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=81a4163ea7eb4bccb489151972100adb"
+  );
+  return response.data.articles
+});
 export const haberSlice = createSlice({
   name: "haberSlice",
   initialState: {
     haberler: [],
     loading: true,
   },
-  reducers:{
-    clearHaber:()=>{
-
-    }
+  reducers: {
+    clearHaber: () => {},
   },
-  extraReducers:()=>{
-
-  }
+  extraReducers: () => {},
 });
 
-export const {clearHaber} = haberSlice.actions
-export default haberSlice.reducer
+export const { clearHaber } = haberSlice.actions;
+export default haberSlice.reducer;
